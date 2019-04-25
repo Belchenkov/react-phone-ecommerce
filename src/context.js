892f0452385e456ec596e1ef48e3895eddd7b141
Алексей Бельченков <u608110@gmail.com>
2019-04-25 17:20:35 +0400

    Output Cart Component

--- src/context.js
+++ src/context.js
@@ -9,7 +9,10 @@ class ProductProvider extends Component {
         cart: [],
         detailProduct: detailProduct,
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     };
 
     componentDidMount() {
@@ -75,6 +78,22 @@ class ProductProvider extends Component {
         });
     };
 
+    increment = id => {
+        console.log('this is increment methods');
+    };
+
+    decrement = id => {
+        console.log('this is decrement methods');
+    };
+
+    removeItem = id => {
+        console.log('item removed');
+    };
+
+    clearCart = () => {
+        console.log('cart was cleared');
+    };
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -82,7 +101,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
